a10cc4c3b38ea2fd0d47b9c688b8810127f1f4fb
r0naldo <ox99709179@gmail.com>
2023-07-18 00:29:28 -0300

    primeiro move arquivos caso tenha mesma extensao

--- OneDrive/code/arquivos/mover_arquivos.py
+++ OneDrive/code/arquivos/mover_arquivos.py
@@ -19,9 +19,9 @@ for file in os.listdir(source_folder):
         # Monta o caminho completo para o arquivo
         file_path = os.path.join(source_folder, file)
         # Move o arquivo para a pasta de destino
-        shutil.move(file_path, destination_folder)
+        shutil.move(file_path, destination_folder)      
 '''
 Esse código assume que a pasta de origem e a pasta de destino já existem. Ele percorre todos os arquivos na pasta de origem, 
 verifica se o arquivo tem a extensão especificada e, em seguida, move o arquivo para a pasta de destino. 
-É importante notar que esse código sobrescreverá arquivos com o mesmo nome na pasta de destino.
+É importante notar que este código sobrescreverá arquivos com o mesmo nome na pasta de destino.
 '''
